781843a51af0334034c95b7f6ecfbb706c92cc7a
DESKTOP-O3HOAJ8\jbin <kjb7907@naver.com>
2016-12-28 02:13:10 +0900

    form에서 제목 내용 받아서 파일로 저장까지

--- server_side_javascript/app_file.js
+++ server_side_javascript/app_file.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var bodyParser = require('body-Parser');
 
+app.use(bodyParser.urlencoded({ extended : true}));
 app.locals.pretty = true;
 app.set('views','views_file');
 app.set('view engine','jade');
@@ -8,7 +11,20 @@ app.set('view engine','jade');
 app.get('/topic/new',function(req,res){
   res.render('new');
 });
-
+app.post('/topic',function(req,res){
+  var title = req.body.title;
+  var description = req.body.description;
+  console.log(title+'타이틀값');
+  console.log(description+'디스크립션값');
+  fs.writeFile('data/'+title,description,function(err){
+    if(err){
+      console.log('에러발생');
+      console.log(err);
+      res.status(500).send('internal server error');
+    }
+    res.send('sucess');
+  });
+});
 app.listen(3000,function(){
   console.log('Connected, 3000 port!');
 });
