0e14d213fcf56b00bf548b219b8394ba0b31e8f7
SMART20107\Administrator <kjb7907@naver.com>
2017-01-20 17:31:38 +0900

    cookie

--- server_side_javascript/app_cookies.js
+++ server_side_javascript/app_cookies.js
@@ -8,7 +8,10 @@ var products = {
   1:{title: 'The history of web 1'},
   2:{title: 'the next web'}
 };
+
+
 app.get('/products', function(req,res){
+  console.log('/products');
   var output='';
   for(var name in products){
     output += `
@@ -20,7 +23,24 @@ app.get('/products', function(req,res){
   res.send(`<ul>${output}</ul><a href ="/">Cart</a>`);
 });
 
+app.get('/cart/:id', function(req,res){
+  var id = req.params.id;
+  if(req.cookies.cart){
+    var cart = req.cookies.cart;
+  } else {
+    var cart={};
+  }
 
+  if(!cart[id]){
+    cart[id] = 0;
+  }
+  cart[id] = parseInt(cart[id])+1;
+  res.cookie('cart',cart)
+  res.redirect('/cart');
+});
+app.get('/cart',function(req,res){
+  res.send('hi cart')
+});
 
 app.get('/count',function(req,res){
   if(req.cookies.count){
@@ -35,6 +55,6 @@ app.get('/count',function(req,res){
 });
 
 
-app.listen(3000, function(){
-  console.log('Connected 3000 port');
+app.listen(3003, function(){
+  console.log('Connected 3003 port');
 });
